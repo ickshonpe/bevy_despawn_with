815c43ddc732d72e5061162124379cee7ac92e1f
Ickshonpe <david.curthoys@googlemail.com>
2022-02-26 12:55:32 +0000

    # changes
        - two new functions retain and retain_recursive that
        selectively despawn entities filtered on a predicate.
    
        - updated README.md to reflect changes

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,26 @@ where
     phantom_component: PhantomData<C>
 }
 
+pub struct Retain<C, P> 
+where 
+    C: Component,
+    P: Fn(&C) -> bool
+{
+    predicate: P,
+    phantom: PhantomData<C>
+}
+
+pub struct RetainRecursive<C, P> 
+where 
+    C: Component,
+    P: Fn(&C) -> bool
+{
+    predicate: P,
+    phantom: PhantomData<C>
+}
+
+
+
 impl <C> Command for DespawnWith<C> 
 where
     C: Component,
@@ -38,9 +58,49 @@ where
     }
 }
 
+impl <C, P> Command for Retain<C, P>
+where
+    C: Component,
+    P: Fn(&C) -> bool + Component,
+{
+    fn write(self, world: &mut World) {
+        let queue: Vec<Entity> = 
+            world.query::<(Entity, &C)>().iter(world)
+            .filter(|(_, c)| !(self.predicate)(c))
+            .map(|(e, _)| e).collect();
+        for entity in queue.into_iter() {
+            world.despawn(entity);
+        }
+    }
+}
+
+impl <C, P> Command for RetainRecursive<C, P>
+where
+    C: Component,
+    P: Fn(&C) -> bool + Component,
+{
+    fn write(self, world: &mut World) {
+        let queue: Vec<Entity> =
+            world.query::<(Entity, &C)>().iter(world)
+            .filter(|(_, c)| !(self.predicate)(c))
+            .map(|(e, _)| e).collect();
+        for entity in queue.into_iter() {
+            despawn_with_children_recursive(world, entity);
+        }
+    }
+}
+
 pub trait DespawnWithExt {
     fn despawn_with<C>(&mut self) where C: Component;
     fn despawn_recursive_with<C>(&mut self) where C: Component;
+    fn retain<C, P>(&mut self, predicate: P)
+    where
+        C: Component,
+        P: Fn(&C) -> bool + Component;
+    fn retain_recursive<C, P>(&mut self, predicate: P)
+    where
+        C: Component,
+        P: Fn(&C) -> bool + Component;
 }
 
 impl DespawnWithExt for Commands<'_, '_> {  
@@ -55,5 +115,31 @@ impl DespawnWithExt for Commands<'_, '_> {
         C: Component
     {
         self.add(DespawnRecursiveWith::<C> { phantom_component: PhantomData } );
+    }
+
+    /// Despawn all entities with a component C that fails to satisfy
+    /// the given predicate.
+    fn retain<C, P>(&mut self, predicate: P)
+    where
+        C: Component,
+        P: Fn(&C) -> bool + Component 
+    {
+        self.add(Retain::<C, P> { 
+            predicate,
+            phantom: PhantomData
+        });
+    }
+
+    /// Recursively despawn all entities with a component C that fails to satisfy
+    /// the given predicate.
+    fn retain_recursive<C, P>(&mut self, predicate: P)
+    where
+        C: Component,
+        P: Fn(&C) -> bool + Component 
+    {
+        self.add(Retain::<C, P> { 
+            predicate,
+            phantom: PhantomData
+        });
     }
 }
